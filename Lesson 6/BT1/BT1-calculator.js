4d77800876888f46d2daa4e633ac58c02e7c5877
VuKimOanh <vukimoanh.tmu@gmail.com>
2023-06-05 17:10:01 +0700

    Merge remote changes

--- Lesson 6/BT1/BT1-calculator.js
+++ Lesson 6/BT1/BT1-calculator.js
@@ -1,35 +1,35 @@
 // Lấy giá trị từ form
 function getValues() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
     return [num1, num2];
 }
 
 // Tính toán và trả về kết quả phép cộng
 function add() {
-    var values = getValues();
-    var result = values[0] + values[1];
+    let values = getValues();
+    let result = values[0] + values[1];
     displayResult(result);
 }
 
 // Tính toán và trả về kết quả phép trừ
 function subtract() {
-    var values = getValues();
-    var result = values[0] - values[1];
+    let values = getValues();
+    let result = values[0] - values[1];
     displayResult(result);
 }
 
 // Tính toán và trả về kết quả phép nhân
 function multiply() {
-    var values = getValues();
-    var result = values[0] * values[1];
+    let values = getValues();
+    let result = values[0] * values[1];
     displayResult(result);
 }
 
 // Tính toán và trả về kết quả phép chia
 function divide() {
-    var values = getValues();
-    var result = values[0] / values[1];
+    let values = getValues();
+    let result = values[0] / values[1];
     displayResult(result);
 }
 
